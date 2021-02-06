e262b34484ac93c0a09dd1127d7b372c9bd46094
Babariya007 <abhaybabariya1@gmail.com>
2021-02-06 17:49:12 +0530

    Final Commit

--- Test/js/user.js
+++ Test/js/user.js
@@ -1,3 +1,4 @@
+//New User
 var array = [];
 
 function newUser() {
@@ -13,9 +14,12 @@ function newUser() {
         Birthday: userBirthday
     }
 
-    if (localStorage.getItem('userarr')) {
-        userarr = JSON.parse(localStorage.getItem('userarr'));
+    if (localStorage.getItem('array')) {
+        userarr = JSON.parse(localStorage.getItem('array'));
     }
+
+    array.push(userData);
+    localStorage.setItem("User", JSON.stringify(array));
 }
 
 
@@ -27,3 +31,61 @@ function getAge(dob) {
 
 }
 document.getElementById('age').innerHTML = getAge(new Date(userBirthday));
+
+
+//Update User
+function updateUser() {
+    var updateUser = JSON.parse(localStorage.getItem('User'));
+    updateUser.forEach(function(obj) {
+        document.getElementById('name').value = obj.Name;
+        document.getElementById('email').value = obj.Email;
+        document.getElementById('pwd').value = obj.Password;
+        document.getElementById('birthdate').value = obj.Birthday;
+        document.getElementById('btnAddUser').innerHTML = "Update User";
+    });
+}
+
+//Fech User
+function fechUser() {
+    var array = localStorage.getItem('User');
+    var dataitems = JSON.parse(array);
+
+    array = dataitems;
+    document.write(`<table class="table">`);
+    document.write(`<thead class="thead-dark">`);
+    document.write(`<tr>`);
+    document.write(`<th scope="col">Name</th>
+    <th scope="col">Email</th>
+    <th scope="col">Password</th>
+    <th scope="col">Birthdate</th>
+    <th scope="col">Age</th>
+    <th scope="col">Action</th>
+    </tr>
+    <tbody>
+    <tr>`);
+
+    for (var i = 0; i < array.length; i++) {
+        document.write('<td>' + array[i].name + '</td>');
+        document.write('<td>' + array[i].email + '</td>');
+        document.write('<td>' + array[i].pswd + '</td>');
+        document.write('<td>' + array[i].birthdate + '</td>');
+        document.write('<td>' + getAge(array[i].birthdate) + '</td>');
+        document.write('<td><button type="button" class="btn btn-primary" onclick="updateUser()"> Update </button></td>');
+        document.write('<td><button type="button" class="btn btn-danger" onclick="deleteUser()"> Delete </button></td>');
+        document.write('</tr>');
+        document.write('</tbody>');
+        document.write('</thead>');
+        document.write('</table>');
+
+    }
+}
+document.getElememtById('userTable').innerHTML = fechUser();
+
+
+//Delete User
+function deleteUser() {
+    var userData = JSON.parse(localStorage.getItem('User'));
+    userData.forEach(function(obj) {
+        localStorage.removeItem(obj.Name);
+    });
+}
